166eb8a6ef4ffea430d8c6be0aaaa90234748bcb
KyawKhantTun <khanttun1999@gmail.com>
2026-05-06 14:51:38 +0630

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6kfvr-6qVSWMwWgp9PfKnGNT3u5K-4zk",
+  authDomain: "learn-arduino-with-ace.firebaseapp.com",
+  projectId: "learn-arduino-with-ace",
+  storageBucket: "learn-arduino-with-ace.firebasestorage.app",
+  messagingSenderId: "100033192953",
+  appId: "1:100033192953:web:3f3f8f155b7afac14c5328"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
